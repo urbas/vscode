4da8879901c54b2147a2b7c49357ac142df702de
Eric Amodio <eamodio@microsoft.com>
2020-01-22 15:42:43 -0500

    Adds refresh for when timeline changes

--- src/vs/workbench/api/browser/mainThreadTimeline.ts
+++ src/vs/workbench/api/browser/mainThreadTimeline.ts
@@ -18,10 +18,13 @@ import {
 } from 'vs/workbench/contrib/timeline/common/timeline';
 import { URI } from 'vs/base/common/uri';
 import { CancellationToken } from 'vs/base/common/cancellation';
+import { Emitter } from 'vs/base/common/event';
 
 @extHostNamedCustomer(MainContext.MainThreadTimeline)
 export class MainThreadTimeline implements MainThreadTimelineShape {
 	private readonly _proxy: ExtHostTimelineShape;
+
+	private readonly _providerEmitters = new Map<string, Emitter<URI | undefined>>();
 
 	constructor(context: IExtHostContext, @ITimelineService private readonly _timelineService: ITimelineService) {
 		this._proxy = context.getProxy(ExtHostContext.ExtHostTimeline);
@@ -36,18 +39,37 @@ export class MainThreadTimeline implements MainThreadTimelineShape {
 
 		const proxy = this._proxy;
 
+		const emitters = this._providerEmitters;
+		let onDidChange = emitters.get(provider.source);
+		// eslint-disable-next-line eqeqeq
+		if (onDidChange == null) {
+			onDidChange = new Emitter<URI | undefined>();
+			emitters.set(provider.source, onDidChange);
+		}
+
 		this._timelineService.registerTimelineProvider({
 			...provider,
+			onDidChange: onDidChange.event,
 			provideTimeline(uri: URI, since: number, token: CancellationToken) {
 				return proxy.$getTimeline(provider.source, uri, since, token);
 			},
-			dispose() { }
+			dispose() {
+				emitters.delete(provider.source);
+				onDidChange?.dispose();
+			}
 		});
 	}
 
 	$unregisterTimelineProvider(source: string): void {
 		console.log(`MainThreadTimeline#unregisterTimelineProvider: source=${source}`);
 		this._timelineService.unregisterTimelineProvider(source);
+	}
+
+	$emitTimelineChangeEvent(source: string, uri: URI | undefined): void {
+		console.log(`MainThreadTimeline#emitChangeEvent: source=${source} uri=${uri?.toString(true)}`);
+
+		const emitter = this._providerEmitters.get(source);
+		emitter?.fire(uri);
 	}
 
 	dispose(): void {
